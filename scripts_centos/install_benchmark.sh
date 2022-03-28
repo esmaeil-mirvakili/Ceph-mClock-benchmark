22659e6dd8ba812380775396aecdfbc9a2617072
esmaeil-mirvakili <smirvaki@ucsc.edu>
2022-03-28 01:17:22 -0700

    cbt install script added

--- scripts_centos/install_benchmark.sh
+++ scripts_centos/install_benchmark.sh
@@ -9,11 +9,12 @@ sudo dnf install -y python3-pip
 sudo dnf install -y git
 
 bash install_ceph.sh
-
+cp cbt_setup.sh "${HOME}"
 cd "${HOME}" || exit
 git clone https://github.com/ceph/cbt.git
+mv cbt_setup.sh cbt/
 cd "${HOME}/cbt" || { echo "CBT clone failed (cbt directory not found)."; exit; }
-pip3 install -r ../requirements.txt
-bash setup.sh
+pip3 install -r requirements.txt
+bash cbt_setup.sh
 #sudo mkdir -p /usr/local/lib/ceph/erasure-code
 #sudo cp -r "${HOME}/ceph/build/lib/*" "/usr/local/lib/ceph/erasure-code"
--- scripts_ubuntu/install_benchmark.sh
+++ scripts_ubuntu/install_benchmark.sh
@@ -14,7 +14,7 @@ cd "${HOME}" || exit
 git clone https://github.com/ceph/cbt.git
 mv ubuntu_cbt_setup.sh "${HOME}/cbt/"
 cd "${HOME}/cbt" || { echo "CBT clone failed (cbt directory not found)."; exit; }
-pip3 install -r ../requirements.txt
+pip3 install -r requirements.txt
 /bin/bash ubuntu_cbt_setup.sh
 sudo ln -s "${HOME}/fio/*" "/usr/local/bin"
 #printf 'export PATH="%s:$PATH"' "${HOME}/fio" >> .bashrc
