dae9aa966554b6dd95305ec2acd1c3af602ca95e
esmaeil-mirvakili <smirvaki@ucsc.edu>
2022-03-28 10:24:20 -0700

    ceph.conf updated

--- scripts_centos/install_benchmark.sh
+++ scripts_centos/install_benchmark.sh
@@ -7,14 +7,28 @@ sudo dnf install -y libffi-devel
 sudo dnf install -y python36-devel
 sudo dnf install -y python3-pip
 sudo dnf install -y git
+sudo yum install targetcli -y
+sudo dnf install -y librados2
+sudo dnf install -y librbd1
+
+sudo rpm --import 'https://download.ceph.com/keys/release.asc' && \
+	ulimit -n 1024 && \
+	yum install -y python3-rbd python3-rados
 
 bash install_ceph.sh
 cp cbt_setup.sh "${HOME}"
 cd "${HOME}" || exit
+mkdir logs
 git clone https://github.com/ceph/cbt.git
 mv cbt_setup.sh cbt/
 cd "${HOME}/cbt" || { echo "CBT clone failed (cbt directory not found)."; exit; }
 pip3 install -r requirements.txt
 bash cbt_setup.sh
+
+curl --silent --remote-name --location https://github.com/ceph/ceph/raw/pacific/src/cephadm/cephadm
+chmod +x cephadm
+sudo ./cephadm add-repo --release nautilus
+sudo ./cephadm install ceph-common
+sudo ./cephadm install python3-rados
 #sudo mkdir -p /usr/local/lib/ceph/erasure-code
 #sudo cp -r "${HOME}/ceph/build/lib/*" "/usr/local/lib/ceph/erasure-code"
--- scripts_centos/register_commands.sh
+++ scripts_centos/register_commands.sh
@@ -1,8 +1,8 @@
 cd ~/ceph/build/bin
 for f in *; do
-  rm -rf "/usr/local/bin/$f"
-  cp -r "$f" "/usr/local/bin"
+  sudo rm -rf "/usr/local/bin/$f"
+  sudo cp -r "$f" "/usr/local/bin"
 done
 cd ~/fio
-rm -rf "/usr/local/bin/fio"
-cp "fio" "/usr/local/bin"
+sudo rm -rf "/usr/local/bin/fio"
+sudo cp "fio" "/usr/local/bin"
