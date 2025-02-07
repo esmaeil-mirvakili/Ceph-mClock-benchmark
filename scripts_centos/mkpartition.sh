c15102133e47ae9d2bf8f036a146589f5bc7b8ba
esmaeil-mirvakili <smirvaki@ucsc.edu>
2025-02-07 10:00:28 -0800

    bug fixed

--- scripts_centos/mkpartition.sh
+++ scripts_centos/mkpartition.sh
@@ -10,11 +10,11 @@ sudo parted -s -a optimal /dev/$slow_storage mkpart osd-device-0-data 0G 20% || 
 sudo parted -s -a optimal /dev/$slow_storage mkpart osd-device-0-block 20% 100% || failed "mkpart 0-block"
 
 echo "Creating osd device journal label on $fast_storage"
-lsblk /dev/sdc
-sudo fdisk -l /dev/sdc
+lsblk /dev/$fast_storage
+sudo fdisk -l /dev/$fast_storage
 sudo parted -s -a optimal /dev/$fast_storage mkpart osd-device-0-wal 0G 20% || failed "mkpart 0-wal"
 sudo parted -s -a optimal /dev/$fast_storage mkpart osd-device-0-db 20% 100% || failed "mkpart 0-db"
 
 echo "partitions by labels:"
-sudo parted -s /dev/sdb print
-sudo parted -s /dev/sdc print
+sudo parted -s /dev/$slow_storage print
+sudo parted -s /dev/$fast_storage print
